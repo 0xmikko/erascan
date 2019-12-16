88cf962dc2ab0cbd0985fac9ee2c26942970fc71
Mikhail Lazarev <mikael.lazarev@gmail.com>
2019-12-17 00:45:49 +0300

    Config to production server changed

--- client/src/config.js
+++ client/src/config.js
@@ -5,6 +5,6 @@
  * Copyright (c) 2019. Mikael Lazarev
  */
 
-export const BACKEND_ADDR = 'http://localhost:8080';
-// export const BACKEND_ADDR = 'https://erascan.info/'
+// export const BACKEND_ADDR = 'http://localhost:8080';
+export const BACKEND_ADDR = 'https://erascan.info/'
 export const FEED_FACTORY_RINKEBY_ADDRESS = '0xDE19C478b2eD51668e36704b2341b81DEBFe2c40'
